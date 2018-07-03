fdf13d573189d7d894d34695af2080cf5c7c30d3
stephen <948592875@qq.com>
2018-07-03 11:47:28 +0800

    resolve confilct

--- src/main/java/dao/TeachDao.java
+++ src/main/java/dao/TeachDao.java
@@ -25,14 +25,14 @@ public interface TeachDao {
 	 * @param year
 	 * @return
 	 */
-<<<<<<< HEAD
-	public List<String> getTeacherIDsByCID(@Param("course_id") String course_id);
+
+	
 
 	public List<String> getYearsByTeacherId(@Param("teacherId")String teacherId);
 
 
-=======
+
 	public List<String> getTeacherIDs(@Param("course_id") String course_id, @Param("year") String year,
 			@Param("semester") int semester);
->>>>>>> c4e8409ca33a6ed292ee547b8f248c28e43e1909
+
 }
--- src/main/java/service/TeachService.java
+++ src/main/java/service/TeachService.java
@@ -15,19 +15,12 @@ public interface TeachService {
 	 */
 	public boolean arrange(String course_id, String teacher_id,String year,int semester);
 
-	/**
-	 * 根据课程信息返回所有任教老师信息
-	 * 
-	 * @param course_id
-	 * @return
-	 */
-<<<<<<< HEAD
-	public List<Teacher> getTeachersByCID(String course_id);
+	
 	
 	
 	public List<String> getYear(String teacherId);
 	
-=======
+
 	public List<Teacher> getTeachers(String course_id,String year,int semester);
->>>>>>> c4e8409ca33a6ed292ee547b8f248c28e43e1909
+
 }
